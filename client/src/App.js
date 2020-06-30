1b552f6bb1f917ba7b950408206fda8b48654c1d
Kenny Huang <khny258@gmail.com>
2020-06-29 21:46:36 -0400

    boilerplate with passport/bcrypt

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import NoMatch from './pages/NoMatch';
+import { user as userAPI } from "./utils/API"
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+	const [user, setUser] = useState({});
+	const [loading, setLoading] = useState(false);
+
+   useEffect(() => {
+      userAPI.authenticate()
+         .then(res => setUser(res.data))
+   }, []);
+   
+	return (
+		<>
+			<div style={{height:40, padding:10, color:"white", lineHeight:"20px"}} className="bg-success">Navbar</div>
+			<Router>
+				<Switch>
+					<Route
+						exact
+						path='/'
+						render={props => (
+							<Login
+								{...{ user, setUser, setLoading }} 
+								{...props}
+							/>
+						)}
+					/>
+					<Route
+						path='/login'
+						render={ () => <Redirect to="/" />}
+					/>
+					<Route exact path='/signup' component={Signup} {...user} loading={loading} />
+					<ProtectedRoute exact path="/home" {...{user, loading, Component: Home} } />
+					<Route component={NoMatch} />
+				</Switch>
+			</Router>
+		</>
+	);
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,23 +1,42 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mongoose = require("mongoose");
+const routes = require("./routes");
+const session = require('express-session')
+const initSession = require("./scripts/initSession");
+const errorHandler = require('./scripts/errorHandler');
 
-// Serve up static assets (usually on heroku)
+// middleware:
+// on every requsest will be called in order.
+
+// initialize session memory.
+app.use(initSession(session));
+
+// parse body middleware.
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Serve static assets (usually on heroku).
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
+// API routes.
+app.use(routes)
+
+// Send every "lost" request to the React app.
+// !Define any API routes before this runs.
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// error handling, last middleware.
+app.use((err, req, res, next) => errorHandler(err, req, res, next));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/passport", { useNewUrlParser: true, useUnifiedTopology: true,  useCreateIndex: true,  });
 
 app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`\n🌎 ==> API server now on http://localhost:${PORT}\n`);
 });
-
-// Connect app to mongo db
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_240t5fjf:password123!!!@ds019101.mlab.com:19101/heroku_240t5fjf";
