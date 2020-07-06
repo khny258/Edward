57a7b2e256b136314624d017321efd624a2a0e7d
Kenny Huang <khny258@gmail.com>
2020-07-05 20:20:49 -0400

    added what we do section

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -8,8 +8,19 @@ function Insights() {
     </div>
     <p>
       <center><img src="https://www.uidownload.com/files/985/679/738/smart-boy-vector-character.jpg"></img></center>
-      <center><b>The smarter sibling of the <a href="https://www.sec.gov/edgar/searchedgar/companysearch.html" target="_blank">SEC's Edgar</a>.</b></center> 
+      <center><h5>The smarter sibling of the <a href="https://www.sec.gov/edgar/searchedgar/companysearch.html" target="_blank">SEC's Edgar</a>.</h5></center> 
     </p>
+    <br />
+    <br />
+    <h2>What We Do</h2>
+    <h5>We share all companies' periodic reports and other forms in a more dynamic way with better user experience so that you can be a well-informed investor.</h5>
+    <br />
+    <br />
+    <img src="https://res.cloudinary.com/test-entreleadership-com/image/upload/v1510602799/6-financial-reports.jpg"></img>
+    <br />
+    <br />
+    <br />
+    <br />
   </container>
   );
 }
