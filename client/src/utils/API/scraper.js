43bc78d3e34b9fd29b1174d9489544e30a916969
Viraj Shetty <viraj.vls@gmail.com>
2020-07-11 01:31:06 -0400

    Working search bar. You will love it!

--- client/src/utils/API/scraper.js
+++ client/src/utils/API/scraper.js
@@ -4,22 +4,12 @@ import axios from "axios";
 export default {
     scrape: function (cik) {
        console.log('reached here')
-      return axios.post("/api/scraper/edgar", cik,
-      {auth: {
-        username: this.props.user,
-        password: this.props.user._id
-      }})
+      return axios.post("/api/scraper/edgar", cik)
     },
     search: function (searchText) {
     console.log('reached here')
    console.log(searchText);
-   return axios.post("/api/scraper/companySearch", searchText)
-  //  {auth: {
-  //    username: this.props.user,
-  //    password: this.props.user._id
-  //  }}
-  
-  
- } 
+   return axios.post("/api/scraper/companySearch", searchText)  
+  } 
 };
   
