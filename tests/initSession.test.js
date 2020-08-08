9da86c209011976a193bed0f4525991f2cd95b69
Kenny Huang <khny258@gmail.com>
2020-08-08 13:07:20 -0400

    cleaned up code

--- tests/initSession.test.js
+++ tests/initSession.test.js
@@ -1,19 +1,19 @@
-const initSession = require('../scripts/initSession')
-const session = require('express-session')
+const initSession = require("../scripts/initSession");
+const session = require("express-session");
 
 describe("Server session tests", () => {
-  describe("Initialization", () => {
-    it("should create a session object", () => {
-      const sessionInstance = initSession(session);
-      console.log(sessionInstance)
+    describe("Initialization", () => {
+        it("should create a session object", () => {
+            const sessionInstance = initSession(session);
+            console.log(sessionInstance);
+        });
     });
-   })
 });
 
-// more server unit tests: 
+// more server unit tests:
 // test structure: AAA -> arrange, act, assert.
 // test ranges: initilazation, positive, negative, error.
-// server side tests: 
+// server side tests:
 //    test internal processes -> scripts
 //    test routes
 //    test errors
